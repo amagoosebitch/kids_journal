3ab6c3df596e743730cdc9943e5526a944a88254
m.shkut <m.shkut@tinkoff.ru>
2024-01-05 01:44:05 +0500

    fix get add organizations

---
+++ web/back/tg_bot/message_replies.py
@@ -0,0 +1,2 @@
+START_COMMAND_REPLY = "Здоров! Как дел?"
+STOP_COMMAND_REPLY = "Бай бай"
---
+++ web/back/tg_bot/settings.py
@@ -0,0 +1,6 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BotSettings(BaseSettings):
+    token: str
+    model_config = SettingsConfigDict(env_prefix='BOT_')

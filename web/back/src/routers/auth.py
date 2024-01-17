6b2e2bff66bf428273c4e964df789a0ab4596258
m.shkut <m.shkut@tinkoff.ru>
2024-01-17 23:40:28 +0500

    kek

--- web/back/src/routers/auth.py
+++ web/back/src/routers/auth.py
@@ -56,46 +56,50 @@ def get_telegram_redirect_widget(telegram_login: str, redirect_settings: Redirec
     return login_widget.redirect_telegram_login_widget(redirect_url=redirect_url)
 
 
-async def login(
-    request: Request,
-    query_params: TelegramAuth = Depends(TelegramAuth),
-    config: BotConfig = Depends(load_bot_config),
-    jwt_settings: JWTSettings = Depends(jwt_settings),
-    employee_service: EmployeeService = Depends(create_employee_service),
-    redirect_settings: RedirectSettings = Depends(create_redirect_settings),
-):
-    telegram_token = config.telegram_token
-    telegram_login = config.telegram_login
-    redirect_widget = get_telegram_redirect_widget(
-        telegram_login=telegram_login, redirect_settings=redirect_settings
-    )
+# async def login(
+#     request: Request,
+#     query_params: TelegramAuth = Depends(TelegramAuth),
+#     config: BotConfig = Depends(load_bot_config),
+#     jwt_settings: JWTSettings = Depends(jwt_settings),
+#     employee_service: EmployeeService = Depends(create_employee_service),
+#     redirect_settings: RedirectSettings = Depends(create_redirect_settings),
+# ):
+#     telegram_token = config.telegram_token
+#     telegram_login = config.telegram_login
+#     redirect_widget = get_telegram_redirect_widget(
+#         telegram_login=telegram_login, redirect_settings=redirect_settings
+#     )
+#
+#     if not query_params.model_dump().get("hash"):
+#         return templates.TemplateResponse(
+#             "login.html",
+#             context={
+#                 "request": request,
+#                 "redirect_telegram_login_widget": redirect_widget,
+#             },
+#         )
+#
+#     try:
+#         validated_data = validate_telegram_data(telegram_token, query_params)
+#         if not validated_data:
+#             return
+#         employee = employee_service.get_by_tg_user_id(str(query_params.id))
+#         token = _create_jwt_token(
+#             jwt_settings=jwt_settings,
+#             employee=employee,
+#         )
+#         return _get_cookie_response(token, redirect_settings)
+#     except TelegramDataIsOutdated:
+#         return HTMLResponse(
+#             "The authentication data is expired.",
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#         )
+#     except TelegramDataError:
+#         return HTMLResponse(
+#             "The request contains invalid data.",
+#             status_code=status.HTTP_400_BAD_REQUEST,
+#         )
 
-    if not query_params.model_dump().get("hash"):
-        return templates.TemplateResponse(
-            "login.html",
-            context={
-                "request": request,
-                "redirect_telegram_login_widget": redirect_widget,
-            },
-        )
 
-    try:
-        validated_data = validate_telegram_data(telegram_token, query_params)
-        if not validated_data:
-            return
-        employee = employee_service.get_by_tg_user_id(str(query_params.id))
-        token = _create_jwt_token(
-            jwt_settings=jwt_settings,
-            employee=employee,
-        )
-        return _get_cookie_response(token, redirect_settings)
-    except TelegramDataIsOutdated:
-        return HTMLResponse(
-            "The authentication data is expired.",
-            status_code=status.HTTP_401_UNAUTHORIZED,
-        )
-    except TelegramDataError:
-        return HTMLResponse(
-            "The request contains invalid data.",
-            status_code=status.HTTP_400_BAD_REQUEST,
-        )
+async def login():
+    pass
